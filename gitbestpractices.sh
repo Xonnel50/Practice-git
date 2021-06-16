990ac42b3397fff1e5e991d550ab7d47d8023c69
xonnel <bullfrig@gmail.com>
2021-06-16 13:04:28 -0400

    Revert "message added"
    This reverts commit 36276f9d59b861004b8f591da280e5903e5fe5b7.

--- gitbestpractices.sh
+++ gitbestpractices.sh
@@ -1,5 +1,5 @@
 Git Best Practices:
-Use at all times
+
     Use branching strategy and pull requests
     Commit once you finish the task. 
     Avoid merge Conflicts.
@@ -8,4 +8,4 @@ Use at all times
     Write Good Commit Messages before committing 
     Try to use git commands rather than GUI tools.
     use ssh url and Personal Access Tokens (PAT) over https 
-    use git rebase over git merge
+    use rebase over git merge
